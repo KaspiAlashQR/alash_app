4226fe4d8141dafa813baac795b37bc5418e3333
Айдос Амангельды <amangeldiaidos660@gmail.com>
2025-12-25 12:01:36 +0500

    Remove all project files for v2 fresh start

--- src/types/react-native-vector-icons.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-native-vector-icons/MaterialCommunityIcons' {
-  import { ComponentType } from 'react';
-  import { IconProps } from 'react-native-vector-icons/Icon';
-  const MaterialCommunityIcons: ComponentType<IconProps>;
-  export default MaterialCommunityIcons;
-}
